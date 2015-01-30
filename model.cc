6dae7f8dfc5536ee2e36a9fa816252e76fc1994c
fallingfrog <fallingfrog@gmail.com>
2015-01-30 16:36:36 -0500

    still some big changes to go

--- model.cc
+++ model.cc
@@ -75,44 +75,82 @@ double model::sample_size(const pattern& p) {
   return total_num_events - p.total_events_at_creation + 1.0;  //might be wrong - time based not event based?
 }
 
-double model::local_prob(const context& cxt, const pattern& p, unsigned t_abs) const {
-  occurrence patt_occ - get_occurrence(p, t_abs);
-  if(get_intersection(cxt.exclude, patt_occ).empty() && (!cxt.single_valued || is_single_valued(get_union(cxt.given, patt_occ)))) {
-    return (p.count + prior_count(p.p.size(), cxt.p_bounds))/sample_size(p);
+double model::local_prob(const context& cxt, pattern& p, unsigned t_abs) const {
+  occurrence patt_occ = get_occurrence(p, t_abs);
+  if(p.last_visit_hash_id != current_visit_hash_id &&
+     get_intersection(cxt.exclude, patt_occ).empty() &&
+     (!cxt.single_valued || is_single_valued(get_union(cxt.given, patt_occ)))) {
+    p.last_visit_hash_id = current_visit_hash_id;
+    return (p.count + prior_count(p.p.size(), cxt.p_bounds))/(sample_size(p) + prior_count(0, cxt.p_bounds));  //this could be made faster but this is elegant
   }
   else
     return 0.0;
 }
 
-double model::global_prob(const context& cxt, const pattern& p, unsigned t_abs) const {
+/*
+Lets say you have a pattern ABCX
+no other examples of ABC
+no other examples of X
+And you want to find P(X|ABC).
+Then P(ABCX)/P(ABC) = 1.
+
+Now my global_prob function will just return global_prob(cxt=ABC, patt=___X) = global_prob(cxt=ABC,patt=ABCX)
+Which in fact is wrong!
+So what did I actually find?  I think I found P(ABCX).
+Maybe I can just normalize: P(ABCX) + P(ABCY) + P(ABCZ) = P(ABC)
+Or: P(ABCX) + P(ABC~X) = P(ABC)
+So then I can calculate P(ABC) from that and --
+
+New observations.
+1) If we visit every pattern a maximum of one time, then probability should be conserved.  Since if we have two patterns ABC and BCD,
+the local counts are assuming ~ABCD in both cases.  If there were a pattern ABCD, then we would have counts for it; since we don't,
+the assumption is that the count of ABCD is zero. --wait, is that a good assumption??
+2) The "base case" patterns actually have a common sub: the pattern containing nothing.  The other base cases are supers of that.
+3) We can find P(ABC&~X) by encoding ~X into the exclusions for the context, and calling global_prob() with that context, the
+base case pattern, and the desired location in time.
+4) For the case where we don't have the single_valued condition, our data will show no examples of A,B at the same time, and
+therefore we will find P(A,B)=0.  The only difference would be in how the prior_counts are calculated, which is something I need
+to revisit.
+5) To find P(X|ABC), we can either find P(ABCX)/P(ABC) or we can find P(ABCX)/(P(ABCX) + P(ABC~X)).  The latter is a bit faster but
+the former is more clean if we want to find many completions at once since we can just find P(ABC) once.
+
+This is all still not quite right.  Doesn't smell right.  I think assumption 1) above is wrong.
+Duh.
+OK, more observations:
+1)  First above is correct *except* that if there is no ABCD, it just means that ABCD is conditionally independent on the subs.
+2)  If cxt represents some givens, then the below algorithm is broken since it will find P(___Q|ABC_) = P_local(_BCQ) + P_local(ABCQ).
+But, if there exists a pattern ABCQ then P_local(_BCQ) = 0.  ABC is given, therefore we are not asking about (~A)BCQ or ABCQ, just ABCQ.
+3)  Because of the conditional independence property, there always exists some (constructed) pattern ABC if ABC is a given.  We just
+have to find out what it is.
+4)  I have no idea how to handle exclusions wrt the multiplied probability method.
+So the below stuff is a good start, but not there yet.  So much more complicated than it looks!
+ */
+
+//Calculates P(cxt & p)
+double model::global_prob(const context& cxt, pattern& p, unsigned t_abs) const {
   double lp = local_prob(cxt, p, t_abs);
   double gp = 0.0;
   if(lp != 0.0) {
-    for(vector<patt_link>::const_iterator p_super = p.super_patterns.begin();p_super != p.super_patterns.end();p_super++) {
-      gp += global_prob(cxt, *(p_super->p_patt), t_abs - p_super->sub_pattern_t_offset)
+    for(auto p_super = p.super_patterns.begin();p_super != p.super_patterns.end();p_super++) {
+      gp += global_prob(cxt, *(p_super->p_patt), t_abs - p_super->sub_pattern_t_offset);
+      //Wrong: if the super overlaps this pattern in the givens, then we use the super only since the existence
+      //of the super implies that the local probability is zero.
     }
   }
 
   return gp;
 }
 
-//TODO: This can be rewritten to be fast
-pattern get_base_level_pattern(unsigned p) const {
-  for(vector<pattern>::const_iterator p_patt = base_level_patterns.begin();p_patt != base_level_patterns.end();p_patt++) {
-    if(!(p_patt->p.empty()) && p_patt->p[0] == p)
-      return *p_patt;
-  }
-
-  //We found nothing that matches; return the prior counts alone
-  pattern patt;
-  patt.count = 0;
-  patt.total_events_at_creation = total_num_events - PRIOR_INTERVAL;
-  patt.p.push_back(p);
-  return patt;
-}
-
-double model::prob(const context& cxt, const event& e) const {
-  return global_prob(cxt, get_base_level_pattern(e.p), e.t);
+double model::prob(context cxt, const event& e) const {
+  occurrence cxt_givens = cxt.givens;
+  occurrence cxt_exclusions = cxt.exclusions;
+  
+  current_visit_hash_id++;
+  double cxt_total_prob = global_prob(cxt, base_level_pattern, e.t);
+  cxt.givens = union(cxt.given, get_occurrence(e));
+  current_visit_hash_id++;
+  double cxt_and_e_prob = global_prob(cxt, base_level_pattern, e.t);
+  return cxt_and_e_prob/cxt_total_prob;
 }
 
 /*
