b756c6faa47766ed7e03e61604fe4ddb3786c4ad
fallingfrog <fallingfrog@gmail.com>
2015-03-06 17:31:56 -0500

    working on model_node network

--- model.cc
+++ model.cc
@@ -15,6 +15,7 @@ FIXME: rewrite all the above.
 2015-03-02
 It occurs to me that I could make all this code quite a bit simpler by eliminating p from the event type, so that patterns consist only of time gaps and not positions.  I could probably merge pattern and occurrence together, which would simplify things a lot, although I would have to rewrite a lot of the matching and is_sub... functions.  I would also have to write a function to translate ascii to events where each bit would be mapped to some sequence like 11___ for zero and 1_1__ for 1.  Because 0 and 1 are mutually exclusive we want each to contain an event the other does not.  We also don't want to get false positives in strings of 1's and 0's.  So then 1101001 would be 11___11___1_1__11___1_1__1_1__11___.  I think the encoding works - I don't see any ambiguity there.  (If they were shorter you would get 11__11__1_1_11__1_1_1_1_11__ which as you can see has big sequences of 1_1_ where things can get confused.)  It makes the configuration tree very much simpler since a completion set would be binary just like the configuration tree so you'd just get a single event and a probability back.  It makes the root node much simpler since there is no second layer above it with just positions.
 Another option is straight up binary - that way we do still need the position, but we still get the result that a completion set has just two options, one of which can be inferred from the other.  And of course since we are just writing down time gaps the above number would be 1414231423231(4).  The binary mode is really a special case of the full position mode.
+FIXME: are sub links *really* necessary??
 
 */
 
@@ -30,29 +31,36 @@ public:
 class model_node {
 public:
   model_node();
-  bool already_visited(unsigned visit_id, int t_abs);
-  void mark_visited(unsigned visit_id, int t_abs);
-  void get_super_patterns(const occurrence& target_occ, list<mn_link> &supers);
-  void reset_super_link(pattern* link_patt, int t_offset);
-  void reset_sub_link(pattern* link_patt, int t_offset);
-  void unlink();
-  
+  void get_super_links(const occurrence& target_occ, list<mn_link> &supers);
+  void get_sub_links(model_node* p_search_node, list< sub_link > &sub_links, int t_abs, unsigned visit_id);
+  void reset_super_link(model_node* link_node, int t_offset);
+  void remove_super_links(model_node* link_node);
+  void free_subtree();
   pattern patt;
   double count;
+private:
+  bool is_compatible(const occurrence& occ, int t_abs) const;
+  bool already_visited(unsigned visit_id, int t_abs) const;
+  void mark_visited(unsigned visit_id, int t_abs);
   unsigned last_visit_id;
   list<unsigned> visited_t_abs; //for this visit id.  We only want to visit each pattern one time with a particular t_abs.
-  
-  //sub, super patterns
   list<patt_link> super_links;
-  list<patt_link> sub_links;
+  unsigned ref_count;
 };
 
 void model_node::model_node() {
   count = 0.0;
   last_visit_id = UINT_MAX;
+  ref_count = 0;
 }
 
-bool model_node::already_visited(unsigned visit_id, int t_abs) {
+//FIXME: for all below functions, pattern& has been replaced by model_node&
+//Returns true if the pattern and the occurrence are not mutually exclusive
+bool model_node::is_compatible(const occurrence& occ, int t_abs) const {
+  return is_single_valued(get_union(occ, get_occurrence(patt, t_abs)));
+}
+
+bool model_node::already_visited(unsigned visit_id, int t_abs) const {
   return (last_visit_id == visit_id && find(visited_t_abs.begin(), visited_t_abs.end(), t_abs) != visited_t_abs.end());
 }
 
@@ -64,8 +72,8 @@ void model_node::mark_visited(unsigned visit_id, int t_abs) {
   visited_t_abs.push_back(t_abs);
 }
 
-void model_node::get_super_patterns(const occurrence& target_occ, list<mn_link> &supers) {
-/  if(patt.p.empty()) { //base case pattern, contains a super for the givens at every possible t
+void model_node::get_super_links(const occurrence& target_occ, list<mn_link> &supers) {
+  if(patt.p.empty()) { //base case pattern, contains a super for the givens at every possible t
     supers.clear();
     for(auto p_e = target_occ.cbegin();p_e != target_occ.cend();p_e++) {
       for(auto p_link = super_links.cbegin(); p_link != super_links.cend();p_link++) {
@@ -77,28 +85,66 @@ void model_node::get_super_patterns(const occurrence& target_occ, list<mn_link> 
   }
 }
 
-void model_node::reset_super_link(model_node* link_node, int t_offset) {
-  for(auto p_link = super_links.begin();p_link != super_links.end();p_link++) {
-    if(p_link->p_patt == link_patt && p_link->t_offset == t_offset)
-      return; //already here
+//C++ always has so much boilerplate.  A little helper class.
+class sub_link {
+public:
+  sub_link(model_node* p_sub_node, list<mn_link>::iterator p_sub_link) { this.p_sub_node = p_sub_node; this.p_super_link = p_super_link; }
+  model_node* p_sub_node;
+  list<mn_link>::iterator p_super_link; //super of this sub
+};
+  
+void model_node::get_sub_links(model_node* p_search_node, list< sub_link > &sub_links, int t_abs, unsigned visit_id) {
+  occurrence target_occ = get_occurrence(patt, 0);
+  
+  if(p_search_node->already_visited(visit_id, t_abs) || !is_sub_occurrence(target_occ, get_occurrence(p_search_node->patt, t_abs)))
+    return;
+  
+  p_search_node->mark_visited(visit_id, t_abs);
+  
+  list<mn_link> search_supers;
+  get_super_links(target_occ, search_supers);
+  for(auto p_link = search_supers.begin();p_link != search_supers.end();p_link++) {
+    if(p_link->p_node == this)
+      subs.push_back(sub_link(p_search_node, p_link));
+    else
+      get_sub_links(p_link->p_node, subs, t_abs + p_link->t_offset, visit_id);
   }
-
-  super_links.push_back(mn_link(link_node, t_offset));
 }
 
-void model_node::reset_sub_link(model_node* link_node, int t_offset) {
-  for(auto p_link = sub_links.begin();p_link != sub_links.end();p_link++) {
+void model_node::reset_super_link(model_node* link_node, int t_offset) {
+  for(auto p_link = super_links.begin();p_link != super_links.end();p_link++) {
     if(p_link->p_node == link_node && p_link->t_offset == t_offset)
       return; //already here
   }
 
-  sub_links.push_back(mn_link(link_node, t_offset));
+  super_links.push_back(mn_link(link_node, t_offset));
+  link_node->ref_count++;
 }
 
-//Reaches out to the nodes linking onto this pattern and deletes those links too
-//If called from the relink() function, this function does NOT affect returned statistics.
-void model_node::unlink() {
-  
+void model_node::remove_super_links(model_node* link_node = NULL) {
+  auto p_link = super_links.begin();
+  while(p_link != super_links.end()) {
+    if(link_node == NULL || p_link->p_node == link_node) {
+      p_link = erase(p_link);
+      link_node->ref_count--;
+    } else
+      p_link++;
+  }
+}
+
+list<mn_link>::iterator p_link model_node::remove_super_link(list<mn_link>::iterator p_link) {
+  p_link->p_node->ref_count--;
+  return erase(p_link);
+}
+
+void model_node::free_subtree() {
+  ref_count--;
+  if(ref_count == 0) {
+    for(auto p_sup = super_links.begin();p_sup != super_links.end();p_current++)
+      p_sup->p_node->free_subtree();
+
+    delete this;
+  }
 }
 
 //Member functions for the model class
@@ -122,55 +168,125 @@ model::model(unsigned memory_constraint) {
 		     
     The offsets for the links up from the root to the base cases are unused because the get_super_links()
     function performs some magic and matches the base cases to the given occ every way that they agree.
+    This makes sense if you parse the meaning of the get_super_links() function to be:
+    "Give me every way that every super pattern of this one matches the current pattern."
+    For all other patterns, you would use the offset and make sure the t values match up.  But in the base
+    case, there is no value of t, so the offset is meaningless, and because there is no constraint on t
+    values, you would return every super with every t_offset.  Writing it this way though makes the code
+    to walk the tree cleaner, since the code is written with the above definition of get_super_links()
+    in mind and doesn't have to know about the 2 cases.
     
     The base cases are not connected to the training set until the first event is added.  When an event
     is added they are manually linked from a base case to the training set and then those links are corrected
     when we call the relink() function. But the reset_links() function does not operate correctly if
-    there is not *some* link from the base cases to the training set.
+    there is not *some* link from the base cases to the training set in the first place.
     
     The training set cannot do double duty as the apex node because the offset from the apex to the training
     set is used to establish the location of the training set in absolute time, which allows us to add events
-    to the training set.
-   */
+    to the training set and automatically adjust the t_offset of the training set in a clean way -the functions
+    to walk the tree don't have to know that the training set is special.
+    
+    After common subsections are factored out, and the training set is delinked from some of them, the tree
+    will look like this:
+                     apex
+                      |
+		 training_set
+	                  |
+                 patt_1 patt_2
+                  |   \ /  |
+                  |    X   |
+                  |   / \  |
+	   base_case_0   base_case_1
+	           \      /
+                    \    /
+                     root
+		     
+    In this scenario note that patt_1 is abstracted from the training set and not set in absolute time.  So
+    it is not reachable from the apex, but only from the root.  That is why we clear the tree from the
+    bottom up.
+  */
+  
   apex = new node();
   training_set = new node();
   base_case_0 = new node();
   base_case_1 = new node();
   root = new node();
 
-  base_case_0.patt.p.push_back(0);
-  base_case_1.patt.p.push_back(1);
+  base_case_0->patt.p.push_back(0);
+  base_case_1->patt.p.push_back(1);
 
-  apex.reset_sub_link(training_set, 0);
-  training_set.reset_super_link(apex, 0);
-  root.reset_super_link(base_case_0, 0);
-  base_case_0.reset_sub_link(root, 0);
-  root.reset_super_link(base_case_1, 0);
-  base_case_1.reset_sub_link(root, 0);
+  training_set->reset_super_link(apex, 0);
+  root->reset_super_link(base_case_0, 0);
+  root->reset_super_link(base_case_1, 0);
+
+  training_set->count = 1.0;
 }
 
 void model::~model() {
-  free_model_tree(root, get_new_visit_id());
+  root->free_subtree();
 }
 
-void model::free_model_tree(model_node* p_current, unsigned visit_id) {
-  
+bool model::ok_to_delink(model_node* n) {
+  if(n == base_case_0 || n == base_case_1 || n == apex || n == root)
+    return false; //This is a bad idea
+
+  return true;
 }
 
+//FIXME: don't know that this needs to be its own function
 void model::relink_common_subsections(const occurrence& occ1, const occurrence& occ2) {
   list<pattern> new_patts;
   convolute(occ1, occ2, new_patts);
   for(auto pn = new_patts.begin();pn != new_patts.end();pn++) {
-    relink(root, get_occurrence(*pn, 0));
+    relink(root, get_occurrence(*pn, false));
   }
 }
 
+//Delinks the given node from the tree and links the subs of the node to the supers directly.
+//This function can affect statistics, if the count is not zero.
+void model_node::delink(model_node* root) {
+  //If a node q is a sub of n, and n is a sub of s, then q is a sub of s.
+  //So when we delink n, all links from subs of n should be copied and shifted
+  //to refer to each of the supers of n.
+
+  //Grab the subs of this node
+  list<sub_link> sub_links;
+  get_sub_links(root, sub_links, 0, m.get_next_visit_id());
+  
+  //For each link in each sub that points back to this node (all the links returned),
+  for(auto p_sub_link = sub_links.cbegin();p_sub_link != sub_links.cend();p_sub_link++) {
+    //Create a link for each super of n
+    for(auto p_super_link = super_links.cbegin();p_super_link != super_links.cend();p_super_link++)
+      p_sub_link->p_node->reset_super_link(p_super_link->p_node, p_super_link->t_offset + p_sub_link->p_super_link->t_offset);
+							  
+    //Erase the original link to this node
+    //FIXME: if this becomes a multithreaded app we might have to have some mechanism to lock nodes so p_sub_link->p_super_link remains valid
+    remove_super_link(p_sub_link->p_super_link);
+  }
+
+  //Now erase the links from this node up
+  remove_super_links();
+
+  if(ref_count != 0)
+    cout << "ERROR: ref count is not zero after delinking node!\n";
+  
+  //Now delete this node
+  delete this;
+}
+
 //Performs a no-touch relink (in other words calling this function does not affect returned statistics)
+//FIXME: this function and find_context are broken, and I need to rethink them.
+/*
+  Try: a separate function to delink and link subs to supers for the removed pattern, and returns the count.
+  Then I can write a link() function which can assume that everything is in a consistent state.  So I need
+  to find the immediate subs and the immediate supers, and then I need to take every link from a listed sub
+  to a listed super and link through the new pattern instead, making sure that such links are unique.
+ */
 void model::relink(const occurrence& occ, bool only_new = true) {
   list<mn_link> supers;
   list<mn_link> subs;
   list<model_node*> siblings;
-  model_node* p_node;
+  model_node* p_node = NULL;
   find_context(root, occ, supers, subs, siblings, p_node, get_next_visit_id());
 
   if(p_node == NULL) {
@@ -180,18 +296,16 @@ void model::relink(const occurrence& occ, bool only_new = true) {
     return; //This is an existing pattern and we are only relinking new ones
 
   //Remove existing links to and from this node
-  p_node->unlink();
+  for(auto p_link = p_node->super_links.begin();p_link != p_node->super_links.end();p_link++)
+    reset_super_link(p_link->p_node, p_node);
   
   //Set the super, sub links  
   p_node->super_links = supers;
   p_node->sub_links = subs;
 
-  //Make sure links in subs, supers back to this pattern are set correctly
-  for(auto p_link = supers.begin();p_link != supers.end();p_link++)
-    reset_sub_link(p_link->p_node, p_node, -(p_link->t_offset));
-  
+  //Make sure links in subs, back to this pattern are set correctly
   for(auto p_link = subs.begin();p_link != subs.end();p_link++)
-    reset_super_link(p_link->p_node, p_node, -(p_link->t_offset));
+    remove_super_links(p_link->p_node, p_node, -(p_link->t_offset));
 
   //Check self and sibling patterns for common subsections
   relink_common_subsections(occ, occ);
@@ -200,7 +314,6 @@ void model::relink(const occurrence& occ, bool only_new = true) {
 }
 
 //p_match is a pointer to the pattern representing occ, unless occ would be a new pattern in which it is NULL.
-//FIXME: not done, does not find offsets
 typedef enum {SUB, SUPER, SIBLING, IDENTITY, NONE} patt_relation;
 patt_relation model::find_context(model_node& p_current,
 				  const occurrence& target_occ,
@@ -284,7 +397,7 @@ void model::sever_link(pattern &patt, sub_link_index) {
 
 //optimize tree to within memory constraints.
 //FIXME: figure out how to balance between memory spent on the apex pattern vs memory spent on the rest
-void model::optimize(const pattern& root, unsigned memory_constraint) {
+void model::optimize(unsigned memory_constraint) {
   //FIXME finish this
 }
 
@@ -292,13 +405,24 @@ void model::optimize(const pattern& root, unsigned memory_constraint) {
 //This DOES affect returned statistics.
 void model::train(const event& e) {
   //Manually insert an event to the end of the training set and link it to a base case pattern
+  int training_set_offset = apex->sub_links[0].t_offset;
+  occurrence training_occ = get_union(get_occurrence(training_set->patt, training_set_offset), get_occurrence(e));
+  training_set->patt = get_pattern(training_occ);
+  model_node* base_case_node;
+  if(e.p)
+    base_case_node = base_case_1;
+  else
+    base_case_node = base_case_0;
+
+  base_case_node->reset_super_link(training_set, e.t - training_set_offset);
+  training_set->reset_sub_link(base_case_node, -(e.t - training_set_offset));
   
-  //make sure common subsections exist
-  relink_common_subsections(apex, apex);
+  //make sure common subsections of the training set exist
+  relink_common_subsections(training_occ, training_occ);
 
   //relink the apex pattern (without modifying counts anywhere)
   //FIXME: this does lots of extra work -really only the portions involving the new event need to be relinked.
-  relink(apex.occ, false);
+  relink(training_occ, false);
   
   //optimize to within memory_constraint.  This DOES affect returned statistics.
   optimize(memory_constraint);
@@ -315,12 +439,6 @@ double model::prior_count(unsigned pattern_length) const {
 //any recursive function that is crawling the tree.
 unsigned model::get_new_visit_id() {
   return ++current_visit_id;
-}
-
-//FIXME: for all below functions, pattern& has been replaced by model_node&
-//Returns true if the pattern and the occurrence are not mutually exclusive
-bool model::is_compatible(const occurrence& occ, const pattern& p, int t_abs) const {
-  return is_single_valued(get_union(occ, get_occurrence(p, t_abs)));
 }
 
 //Returns the size of the sample space containing p
