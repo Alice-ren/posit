d49851a27fcd42800ac8adef0892ce0c46a7af99
Sean Carter <fallingfrog@gmail.com>
2015-03-12 22:56:23 -0400

    Made configuration not depend on model code anymore via a std::function.  Also made to work with new pattern class.  Also now assumes event position is one of {0,1}

--- configuration.cc
+++ configuration.cc
@@ -5,59 +5,34 @@
 
 #include "configuration.hh"
 
-typedef enum {EVENT, COMPLEMENT} t_node_type;
-
 //Internal storage for a configuration space
 class config_node {
 public:
   config_node(config_node* super, double prob, event e) {
+    if(super == NULL && e.t != 0)
+      cout << "ERROR: first event in configuration space not at t=0";      
+      
     this->super = super;
     sub_given = NULL;
-    sub_excluded = NULL;
-    this->remaining_completions = NULL;
+    sub_complement = NULL;
     this->prob = prob;
-    prob_current = true;
+    prob_needs_update = false;
     this->e = e;
-    node_type = EVENT;
   }
 
-  config_node(config_node* super, double prob, completion_set* remaining_completions) {
-    this->super = super;
-    sub_given = NULL;
-    sub_excluded = NULL;
-    this->remaining_completions = remaining_completions;
-    this->prob = prob;
-    prob_current = true;
-    node_type = COMPLEMENT;
-  }
-  
-  void expand(const model& m, event_bounds t_bounds) { //FIXME: requiring a model here is not elegant
-    free_subs(); //just in case there are some already
-    if(node_type == EVENT || (remaining_completions == NULL && prob != 0.0)) {
-      remaining_completions = new completion_set;
-      *remaining_completions = m.get_first_order_completions(givens(), t_bounds); //FIXME: this is awkward.
-    }
-    
-    completion cmp = remaining_completions->get_next_completion(m);
-    
-    if(remaining_completions->remaining_prob() == 0.0) {
-      delete remaining_completions;
-      sub_excluded = new config_node(this, 0.0, NULL);
-    } else
-      sub_excluded = new config_node(this, remaining_completions->remaining_prob(), remaining_completions);
-    
-    sub_given = new config_node(this, cmp.prob, cmp.e);
-    
-    remaining_completions = NULL; //We only want one pointer to this, otherwise we could double-delete
-    node->invalidate_prob();
+  void expand(int t, double given_prob, double complement_prob) {
+    free_subs(); //just in case we are trying to do something wierd
+    sub_given = new config_node(this, given_prob, event(t, 1));
+    sub_complement = new config_node(this, complement_prob, event(t, 0));
+    invalidate_prob();
   }
 
   bool expanded() const {
-    return (node->sub_given != NULL && node->sub_excluded != NULL);
+    return (node->sub_given != NULL && node->sub_complement != NULL);
   }
 
   invalidate_prob() {
-    prob_current = false;
+    prob_needs_update = true;
     if(super != NULL)
       super->invalidate_prob();
   }
@@ -67,36 +42,30 @@ public:
       sub_given->free_subs();
       delete sub_given;
     }
-    if(sub_excluded != NULL) {
-      sub_excluded->free_subs();
-      delete sub_excluded;
+    if(sub_complement != NULL) {
+      sub_complement->free_subs();
+      delete sub_complement;
     }
-    if(remaining_completions != NULL)
-      delete remaining_completions;
 
     sub_given = NULL;
-    sub_excluded = NULL;
-    remaining_completions = NULL;
+    sub_complement = NULL;
   }
-
-  occurrence givens() { //Not the most efficient way to write this
-    occurrence occ;
-    if(node_type == EVENT)
-      occ = get_occurrence(e);
-
+  
+  //Note the assumption that the first event is at t=0 and all other events have t_offset relative to that
+  occurrence givens() const { //Not the most efficient way to write this
     if(super != NULL)
-      return get_union(occ, super->givens());
+      return get_union(super.givens(), e.t, pattern(e.p));
     else
-      return occ;
+      return pattern(e.p);
   }
 
   double prob() {
-    if(!prob_current) {
+    if(prob_needs_update) {
       prob = 0.0; //so there is no path where prob is undefined
       if(expanded())
-	prob = sub_given->prob() + sub_excluded->prob();
+	prob = sub_given->prob() + sub_complement->prob();
       
-      prob_current = true;
+      prob_needs_update = false;
     }
   
     return prob;
@@ -114,7 +83,7 @@ public:
     return 0.0;
   }
 
-  event sub_event() {
+  event sub_event() const {
     if(expanded())
       return sub_given->e;
     else {
@@ -128,8 +97,8 @@ public:
     if(expanded()) {
       sub_given->free_subs();
       sub_given->prob = 0.0;
-      sub_given->prob_current = true;
-      return sub_exclusion;
+      sub_given->prob_needs_update = false;
+      return sub_complement;
     }
     return NULL;
   }
@@ -137,9 +106,9 @@ public:
   //Mark the complement as probability zero
   config_node* choose_event() {
     if(expanded()) {
-      sub_exclusion->free_subs();
-      sub_exclusion->prob = 0.0;
-      sub_exclusion->prob_current = true;
+      sub_complement->free_subs();
+      sub_complement->prob = 0.0;
+      sub_complement->prob_needs_update = false;
       return sub_given;
     }
     return NULL;
@@ -167,43 +136,37 @@ public:
     }
   }
 
-  void configuration::print(unsigned tab_layers) const {
+  void configuration::print(unsigned tab_layers) {
     if(expanded())
       cout << "-->";
     else
       cout << "   ";
   
-    cout << setw(10) << prob << " " << setw(10) << quality << "\t";
+    cout << setw(10) << prob() << " " << setw(10) << quality << "\t";
     for(unsigned i = 0;i < tab_layers;i++)
       cout << "\t";
     cout << "Given: " << endl;
-    print_occurrence(givens());
+    print_pattern(givens());
   
     if(expanded()) {
       sub_given->print(tab_layers + 1);
-      sub_excluded->print(tab_layers + 1);
+      sub_complement->print(tab_layers + 1);
     }	
-  }
-  
-  t_node_type get_node_type() const {
-    return node_type;
   }
   
 private:
   double prob;
-  bool prob_current;
+  bool prob_needs_update;
   config_node* sub_given;
-  config_node* sub_excluded;
+  config_node* sub_complement;
   config_node* super;
-  t_node_type node_type;
   event e;
-  completion_set* remaining_completions;
 };
 
 
 //Member functions of the configuration space
 configuration_space::configuration_space() {
-  root_node = new config_node(NULL, 1.0, NULL);
+  root_node = new config_node(NULL, 1.0, event(1, 0));
   fulcrum_node = root_node;
 }
 
@@ -217,24 +180,27 @@ void configuration_space::choose_event() {  //Make the given node the root node
     fulcrum_node = fulcrum_node->choose_event();
 }
 
-void configuration_space::choose_complement() { //Mark the given as prob=0.00 and the exclusion as 1.00 without invalidating the whole tree
+void configuration_space::choose_complement() { //Mark the given as prob=0.00 and the complement as 1.00 without invalidating the whole tree
   if(fulcrum_node->expanded())
     fulcrum_node = fulcrum_node->choose_complement();
 }
 
-double configuration_space::next_event_cond_prob() { //conditional probability
+double configuration_space::next_event_cond_prob() const { //conditional probability
   return fulcrum_node->sub_event_cond_prob();
 }
 
-//FIXME: what if the user did not do any predicting beforehand?
-event configuration_space::next_event() {
+event configuration_space::next_event() const {
+  if(!fulcrum_node->expanded())
+    cout << "ERROR: fulcrum node is not expanded; no next event to return.\n";
+  
   return fulcrum_node->sub_event();
 }
 
-void configuration_space::predict(const model &m, event_bounds t_bounds, unsigned max_events, double predict_time_limit) {
+void configuration_space::predict(unsigned max_events, double predict_time_limit, function<completion_set(pattern)> get_first_order_completions) {
   for(unsigned i=0;i<max_events;i++) { //FIXME: Insert timing code.
     config_node* best_node = root.highest_probability_leaf_node();
-    best_node->expand(m);
+    completion_set cmp = get_first_order_completions(best_node->givens());
+    best_node->expand(cmp.t_offset, cmp.event_prob, cmp.complement_prob);
   }
 }
 
--- configuration.hh
+++ configuration.hh
@@ -3,9 +3,7 @@
       Configuration struct and related functions to walk a tree defining a configuration space
 ******/
 
-#include "occurrence.hh"
-#include "model.h"
-#include <list>
+#include "pattern.hh"
 #include <iostream>
 #include <iomanip>
 
@@ -22,9 +20,9 @@ public:
   ~configuration_space();
   void choose_event();
   void choose_complement();
-  double next_event_cond_prob();
-  event next_event();
-  void predict(const model &m, event_bounds t_bounds, unsigned max_events, double predict_time_limit);
+  double next_event_cond_prob() const;
+  event next_event() const;
+  void predict(unsigned max_events, double predict_time_limit, function<completion_set(pattern)> get_first_order_completions);
 private:
   config_node* root_node;
   config_node* fulcrum_node;
